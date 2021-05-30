f3e9b217caa4cff0201c96cd34fbac0f211723e9
Dickid <Dickid@Jxgm.com>
2021-05-30 19:14:05 +0800

    third commit

--- src/main/java/com/jxgm/git/GitTest.java
+++ src/main/java/com/jxgm/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git1");
         System.out.println("Hello Git2");
+        System.out.println("Hello Git3");
     }
 }
