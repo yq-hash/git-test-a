2471742f55de47b91f110eb8a963dcf10de064bb
Dickid <Dickid@Jxgm.com>
2021-05-30 19:28:34 +0800

    merge-master commit

--- src/main/java/com/jxgm/git/GitTest.java
+++ src/main/java/com/jxgm/git/GitTest.java
@@ -9,7 +9,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git1");
         System.out.println("Hello Git2");
-        System.out.println("Hello Git3");
+        System.out.println("Hello Git3-new");
         System.out.println("Hello Git4");
     }
 }
