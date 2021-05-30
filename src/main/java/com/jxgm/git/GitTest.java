f4a5f484993a0610ad5a63fbb4c2bb83842a903c
Dickid <Dickid@Jxgm.com>
2021-05-30 19:09:54 +0800

    first commit

---
+++ src/main/java/com/jxgm/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.jxgm.git;
+
+/**
+ * Date:2021/5/30
+ * Author:杨祺
+ * Description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
